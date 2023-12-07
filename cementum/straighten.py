8cd00b98489327d0f4ecaabe48dce99442c1ab23
Richard Lane <richard.lane@bristol.ac.uk>
2023-12-07 16:45:13 +0000

    remove edges from corner of image

--- cementum/straighten.py
+++ cementum/straighten.py
@@ -15,4 +15,28 @@ def find_edges(image: np.ndarray) -> np.ndarray:
     :returns: image with edges indicated
 
     """
-    return Image.fromarray(image).filter(ImageFilter.FIND_EDGES)
+    edges = np.asarray(Image.fromarray(image).filter(ImageFilter.FIND_EDGES)).copy()
+
+    # If the edge is on the outside of the image, remove it
+    edges[:, 0] = 0
+    edges[:, -1] = 0
+    edges[0] = 0
+    edges[-1] = 0
+
+    return edges
+
+
+def fit_edges(edges: np.ndarray) -> np.ndarray:
+    """
+    Given an array representing edge locations
+
+    """
+    assert np.unique(edges).size == 2, "Edges must be binary"
+
+    # Find the x and y co-ordinates of the two edges
+
+    # Correct for any large jumps in the x-coordinate
+
+    # Fit polynomials to the edges
+
+    # Return these polynomials
