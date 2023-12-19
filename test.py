b60fff882fce4b70c231f2cc6e49eb87f7b8d327
Richard Lane <richard.lane@bristol.ac.uk>
2023-12-19 15:53:42 +0000

    test script to reproduce bug

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
 import pickle
-import numpy as np
 import matplotlib.pyplot as plt
 
 import multiProcStraight
 
 # Read in a mask + image
-with open("cementum_data.pickle", "rb") as data:
-    _, _, test_images, test_masks = pickle.load(data)
-image, mask = test_images[0], test_masks[0]
+with open("image_and_mask.pickle", "rb") as data:
+    image, mask = pickle.load(data)
+
+# Display them
+fig, axis = plt.subplots()
+axis.imshow(image, cmap="gray")
+axis.imshow(mask, alpha=0.3)
+plt.show()
 
 # Straighten them with Elis' code
-pts1 = np.column_stack(np.where(mask == 1))
+straightened = multiProcStraight.straightenTest(mask, image)
 
-straight = multiProcStraight.straightenTest(pts1, image)
-plt.imshow(straight)
-
-plt.show()
+# Error message:
+# Traceback (most recent call last):
+#   File "/home/mh19137/cementum/test.py", line 17, in <module>
+#     straightened = multiProcStraight.straightenTest(mask, image)
+#   File "/home/mh19137/cementum/multiProcStraight.py", line 253, in straightenTest
+#     min_a1_x, max_a1_x = min(a1[:,0]), max(a1[:,0])
+# IndexError: too many indices for array: array is 1-dimensional, but 2 were indexed
