91dc7ffa23ee8206a848f2b27c53eca7c0aef75c
Richard Lane <richard.lane@bristol.ac.uk>
2024-02-02 11:39:58 +0000

    skel

---
+++ cementum/pipeline.py
@@ -0,0 +1,18 @@
+"""
+Putting things together
+
+"""
+
+
+def count_layers(image_path: str, mask_path: str) -> list[int]:
+    """
+    Count the number of layers in each row in an image
+
+    """
+    # Load the image and mask
+    # Straighten the image
+    # Cut off white columns and black rows
+    # Crop out cementum
+    # Contrast normalisation
+    # Ridge detection
+    # Count layers
