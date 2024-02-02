d14e640dd6b957a4928e1a4569ce2422d8c358c8
Richard Lane <richard.lane@bristol.ac.uk>
2024-02-02 11:55:40 +0000

    cropping and normalisatino

--- cementum/pipeline.py
+++ cementum/pipeline.py
@@ -5,8 +5,9 @@ Putting things together
 from dataclasses import dataclass
 
 import matplotlib.pyplot as plt
+from skimage.filters import gaussian
 
-from . import straighten
+from . import straighten, fit_cementum, filter
 
 
 @dataclass
@@ -16,9 +17,18 @@ class Params:
 
     """
 
+    # Straightening
     n_y: int
     n_x: tuple[int, int, int]
     straighten_order: int
+
+    # Cementum detection
+    domain_length: int
+
+    # Contrast adjustment
+    filter_angle: float = 0.0
+    filter_sigma: float
+    blur_sigma: float
 
 
 def count_layers(image_path: str, mask_path: str, params: Params) -> list[int]:
@@ -38,7 +48,22 @@ def count_layers(image_path: str, mask_path: str, params: Params) -> list[int]:
     )
 
     # Cut off white columns and black rows
+    straight_image, _ = straighten.remove_white_cols(straight_image, straight_image)
+    straight_image = straighten.remove_padding(straight_image)
+
     # Crop out cementum
+    col_intensity = np.mean(straight_image, axis=0)
+    dentin = fit_cementum.find_boundary(col_intensity, params.domain_length)
+    background = fit_cementum.find_background(col_intensity)
+    cropped_image = straight_image[:, background:dentin]
+
     # Contrast normalisation
+    blurred_image = gaussian(
+        filter.steerable_filter(
+            cropped_image, theta=params.filter_angle, sigma=params.filter_sigma
+        ),
+        sigma=params.blur_sigma,
+    )
+
     # Ridge detection
     # Count layers
